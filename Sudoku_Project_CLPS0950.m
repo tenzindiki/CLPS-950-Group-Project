4bd7d88bfd1852c7e5ee71b9f05b0ddd0de9687e
ariella-reynolds <ariella_reynolds@brown.edu>
2025-03-17 14:50:44 -0400

    trying to figure out new way of randomizing sudoku board

--- Sudoku_Project_CLPS0950.m
+++ Sudoku_Project_CLPS0950.m
@@ -1,5 +1,6 @@
 function sudoku_grid = Sudoku_Project_CLPS0950()
 sudoku_grid = zeros(9); % initialization of grid
+sudoku_grid = fillSudoku(sudoku_grid);
 num_of_blanks = [20,30,40,50,60]; % possible number of blanks
 fprintf('Welcome to our Sudoku!\n'); % welcome message
 
@@ -14,31 +15,6 @@ disp(sudoku_grid); % displays sudoku grid
 end
 
 function grid = fillSudoku(grid)
-    [row, col] = find(grid == 0, 1); % finds the first empty cell in grid (0)
     nums = randperm(9); % randomizes numbers 1-9
-    for num = nums % places numbers in grid
-        if isValid(grid, row, col, num)
-            grid(row, col) = num;
-            if all(grid(:) > 0) || ~isempty(fillSudoku(grid))
-                return;
-            end
-            grid(row, col) = 0;
-        end
-    end
-    grid = []; % backtracks if no valid number is found
-end
+    M(1,4:6) = N(2,:) = Q(1:3);
 
-function valid = isValid(grid, row, col, num) % validates numbers in grid
-    valid = ~ismember(num, grid(row, :)) && ...
-            ~ismember(num, grid(:, col)) && ...
-            ~ismember(num, grid(3 * floor((row - 1) / 3) + (1:3), 3 * floor((col - 1) / 3) + (1:3)));
-end
-
-function grid = create_blanks(grid, count) 
-    indices = randperm(81, count); % creates blanks at random intervals in grid
-    for ii = 1:length(indices)
-        row = ceil(indices(ii) / 9);
-        col = mod(indices(ii) - 1, 9) + 1;
-        grid(row, col) = 0; % populates field with a '0' if blank
-    end
-end
